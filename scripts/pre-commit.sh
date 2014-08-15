76c5ad7cbe190893c67f8321965c2c1eafe1a5f2
c-w <clemens.wolff+git@gmail.com>
2014-08-15 18:28:15 +0100

    Refactor: extract some functions

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 
-STAGED="$(git diff-index --name-only --cached HEAD | uniq)"
+################################################################################
+#                           utility functions
+################################################################################
+
+ncols() { awk -F'\t' '{print NF}'; }
+staged() { git diff-index --name-only --cached HEAD | uniq; }
+tsvfilter() { grep -i '.tsv$'; }
+warning() { echo "$@" 1>&2; }
+space2tab() { sed -i '/'"    "'/{s//'"\t"'/g;h};${x;/./{x;q1};x}' "$1"; }
+
+
+################################################################################
+#                           start of hook
+################################################################################
 
 # replace spaces with tabs in TSV files
-for TSV_FILE in $(echo "${STAGED}" | grep -i '.tsv$'); do
-    sed -i '/'"    "'/{s//'"\t"'/g;h};${x;/./{x;q1};x}' "${TSV_FILE}" \
-        || echo "Replaced spaces for tabs in ${TSV_FILE}" 1>&2
+for TSV_FILE in $(staged | tsvfilter); do
+    space2tab "${TSV_FILE}" || warning "Replaced spaces for tabs in ${TSV_FILE}"
     git add "${TSV_FILE}"
 done
 
 # make sure that all the TSV files have the right number of columns
-for TSV_FILE in $(echo "${STAGED}" | grep -i '.tsv$'); do
-    FILE_NCOLS="$(head -1 ${TSV_FILE} | awk -F'\t' '{print NF}')"
+for TSV_FILE in $(staged | tsvfilter); do
+    FILE_NCOLS="$(head -1 ${TSV_FILE} | ncols)"
     LINE_NO=0
     while read LINE; do
         LINE_NO=$((LINE_NO + 1))
-        LINE_NCOLS=$(echo "${LINE}" | awk -F'\t' '{print NF}')
+        LINE_NCOLS=$(echo "${LINE}" | ncols)
         if [ "${LINE_NCOLS}" -ne "${FILE_NCOLS}" ]; then
-            echo "Error on line ${LINE_NO} in file '${TSV_FILE}'" 1>&2
-            echo "Should have ${FILE_NCOLS} columns but has ${LINE_NCOLS}" 1>&2
+            warning "Error on line ${LINE_NO} in file '${TSV_FILE}'"
+            warning "Should have ${FILE_NCOLS} columns but has ${LINE_NCOLS}"
             exit 1
         fi
     done < "${TSV_FILE}"
