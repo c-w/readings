a2ff6415b8ca0099d0fb1268226484cc57e4912b
c-w <clemens.wolff+git@gmail.com>
2014-08-15 18:13:03 +0100

    Add hook to make sure TSV files don't miss columns
    NB: This hook assumes that the first line of every TSV file is a header that
    specifies the schema of the data to come.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -9,4 +9,19 @@ for TSV_FILE in $(echo "${STAGED}" | grep -i '.tsv$'); do
     git add "${TSV_FILE}"
 done
 
+# make sure that all the TSV files have the right number of columns
+for TSV_FILE in $(echo "${STAGED}" | grep -i '.tsv$'); do
+    FILE_NCOLS="$(head -1 ${TSV_FILE} | awk -F'\t' '{print NF}')"
+    LINE_NO=0
+    while read LINE; do
+        LINE_NO=$((LINE_NO + 1))
+        LINE_NCOLS=$(echo "${LINE}" | awk -F'\t' '{print NF}')
+        if [ "${LINE_NCOLS}" -ne "${FILE_NCOLS}" ]; then
+            echo "Error on line ${LINE_NO} in file '${TSV_FILE}'" 1>&2
+            echo "Should have ${FILE_NCOLS} columns but has ${LINE_NCOLS}" 1>&2
+            exit 1
+        fi
+    done < "${TSV_FILE}"
+done
+
 exit 0
