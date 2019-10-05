be897741087ebae1a75f1314662339f23674f2ea
Clemens Wolff <clemens.wolff+git@gmail.com>
2019-10-05 13:27:40 -0400

    Replace CDNJS links with imports (#9)
    * Replace marked CDNJS link with npm import
    
    * Replace js-cookie CDNJS link with npm import

--- src/App.vue
+++ src/App.vue
@@ -151,7 +151,10 @@ const queryParameters = parseQueryParameters();
 /* main                                                                       */
 /*----------------------------------------------------------------------------*/
 
-/* global Cookies, marked, Materialize, $ */
+/* global Materialize, $ */
+
+import * as Cookies from 'js-cookie';
+import * as marked from 'marked';
 
 export default {
   data: function() {
